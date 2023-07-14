a2a048cbbdba68274a08576a9e538511083c380b
millyanne93 <nmillyanne20@gmail.com>
2023-07-14 11:38:11 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,17 @@
 int main(void)
 {
 int lowercase = 'a';
+int uppercase = 'A';
+
 while (lowercase <= 'z')
 {
 	putchar(lowercase);
 	lowercase += 1;
+}
+while (uppercase <= 'Z')
+{
+	putchar(uppercase);
+	uppercase += 1;
 }
 putchar('\n');
 return (0);
