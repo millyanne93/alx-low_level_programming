9393df9130894c10acd5c2c95758d57e04d794c9
millyanne93 <nmillyanne20@gmail.com>
2023-07-14 12:34:32 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,14 @@ int main(void)
 {
 	int lowercase = 'a';
 
-	while (lowercas <= 'z')
+	while (lowercase <= 'z')
 	{
-		if lowercase == 'e' || lowercase == 'q'
-			lowercase += 1;
+		if (lowercase != 'e' && lowercase != 'q')
+		{
+			putchar(lowercase);
+		}
+			lowercase++;
 	}
-	else
-	{
-		putchar(lowercase);
-		lowercase += 1;
-	}
-}
 putchar('\n');
 return (0);
 }
-
