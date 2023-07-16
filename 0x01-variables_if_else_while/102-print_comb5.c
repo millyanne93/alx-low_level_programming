ccc07eba7dc491ab8eb9249c34da167bcbc0f954
millyanne93 <nmillyanne20@gmail.com>
2023-07-16 08:19:59 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,9 +7,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 99; j++)
 		{
 			if (i != j)
 			{
@@ -19,7 +19,7 @@ int main(void)
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
 				putchar(',');
-				if (i != 9 || j != 8)
+				if (i != 99 || j != 98)
 				{
 					putchar(' ');
 				}
