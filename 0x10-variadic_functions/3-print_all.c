f35a40eca474bf08844a9c8d5c194233ab97ac18
millyanne93 <nmillyanne20@gmail.com>
2023-08-18 13:51:56 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,12 +4,13 @@
 
 /**
  * print_all - prints anything
- * @format: types to print
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
 	int i = 0;
-	char *str;
+	char *str, *sep = "";
+
 	va_list list;
 
 	va_start(list, format);
@@ -20,26 +21,25 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%c", va_arg(list, int));
+					printf("%s%c", sep, va_arg(list, int));
 					break;
 				case 'i':
-					printf("%d", va_arg(list, int));
+					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
-					printf("%f", va_arg(list, double));
+					printf("%s%f", sep, va_arg(list, double));
 					break;
 				case 's':
 					str = va_arg(list, char *);
 					if (!str)
 						str = "(nil)";
-					printf("%s", str);
+					printf("%s%s", sep, str);
 					break;
 				default:
 					i++;
 					continue;
 			}
-			if (format[i + 1])
-				printf(", ");
+			sep = ", ";
 			i++;
 		}
 	}
