d8e88c9a0012a48deb03a2c25d6821bc962723c9
millyanne93 <nmillyanne20@gmail.com>
2023-10-12 12:13:43 -0700

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,55 +1,44 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at
- * a given position
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
  *
- * @h: head of the list
- * @idx: index of the new node
- * @n: value of the new node
- * Return: the address of the new node, or NULL if it failed
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *new = NULL;
-	dlistint_t *head;
-	unsigned int i;
-	dlistint_t *temp;
+	dlistint_t *temp = *head;
+	dlistint_t *h2;
+	unsigned int i = 0;
 
-	if (idx == 0)
-		new = add_dnodeint(h, n);
-	else
+	if (temp != NULL)
+		while (temp->prev != NULL)
+			temp = temp->prev;
+	while (temp != NULL)
 	{
-		head = *h;
-		i = 1;
-		if (head != NULL)
-			while (head->prev != NULL)
-				head = head->prev;
-		temp = head;
-
-		while (temp != NULL)
+		if (i == index)
 		{
-			if (i == idx)
+			if (i == 0)
 			{
-				if (temp->next == NULL)
-					new = add_dnodeint_end(h, n);
-				else
-				{
-					new = malloc(sizeof(dlistint_t));
-					if (new != NULL)
-					{
-						new->n = n;
-                        new->next = temp->next;
-			new->prev = temp;
-			temp->next->prev = new;
-			temp->next = new;
-					}
-				}
-				break;
+				*head = temp->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
 			}
-			temp = temp->next;
-			i++;
+			else
+			{
+				h2->next = temp->next;
+				if (temp->next != NULL)
+					temp->next->prev = h2;
+			}
+			free(temp);
+			return (1);
 		}
+		h2 = temp;
+		temp = temp->next;
+		i++;
 	}
-	return (new);
+	return (-1);
 }
