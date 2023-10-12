f4ee2b79fb1eef202de3f3fa2c158c11bebf2ca3
millyanne93 <nmillyanne20@gmail.com>
2023-10-12 11:46:43 -0700

    password

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = NULL;
+	dlistint_t *head;
+	unsigned int i;
+	dlistint_t *temp;
+
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		temp = head;
+
+		while (temp != NULL)
+		{
+			if (i == idx)
+			{
+				if (temp->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+                        new->next = temp->next;
+			new->prev = temp;
+			temp->next->prev = new;
+			temp->next = new;
+					}
+				}
+				break;
+			}
+			temp = temp->next;
+			i++;
+		}
+	}
+	return (new);
+}
